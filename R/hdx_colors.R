f52d8b7a58bdcefe47fdaa58c6e2b7eb90f6fc10
Seth Caldwell <caldwellst@gmail.com>
2022-07-20 14:45:08 +0100

    Add hex code access and name functions

--- R/hdx_colors.R
+++ R/hdx_colors.R
@@ -1,7 +1,12 @@
 #' Hex values for HDX colors
 #'
 #' `hdx_colors()` conveniently returns a vector of hex values for specified
-#' color ramps. Full values can be found in [gghdx::hdx_color_list].
+#' color ramps. Full values can be found in [gghdx::hdx_color_list]. If you
+#' know the name of the color you want, such as "sapphire-hdx", you can use
+#' `hdx_hex(c("sapphire-hdx"))` to directly access the hex code.
+#'
+#' All valid color names are in the named vector returned by `hdx_colors()` or
+#' accessible in the convenient `hdx_colors_names()`.
 #'
 #' @param colors Specified color ramps to return. Some set of "sapphire",
 #'     "mint", "tomato", and "gray. By default returns all colors.
@@ -35,3 +40,28 @@ hdx_colors <- function(colors = c("sapphire", "mint", "tomato", "gray")) {
 #' @rdname hdx_color
 #' @export
 hdx_colours <- hdx_colors
+
+#' @rdname hdx_color
+#' @param color_names Vector of color names. Valid values are all available
+#'
+#' @export
+hdx_hex <- function(color_names) {
+  if (!all(color_names %in% hdx_colors_names())) {
+    stop(
+      "Not all color names are valid. Check `hdx_colors_names()` for",
+      "all valid values.",
+      call. = FALSE
+    )
+  }
+  hdx_colors()[color_names]
+}
+
+#' @rdname hdx_color
+#' @export
+hdx_colors_names <- function() {
+  unname(names(hdx_colors()))
+}
+
+#' @rdname hdx_color
+#' @export
+hdx_colours_names <- hdx_colors_names
